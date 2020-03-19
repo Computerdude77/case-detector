5347e4b32cb063a20f2a6ad2d1e8e7d22e18c6c6
Caleb <calebljeffrey@gmail.com>
2020-03-18 22:41:28 -0500

    Fixed parameter error

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include <iostream> 
 #include <cctype> 
 using namespace std; 
+bool legal_int(char *str) {
+    while (*str)
+        if (!islower(*str++))
+            return false;
+    return true;
+}
+
+bool legal_num(char *str) {
+    while (*str)
+        if (!isdigit(*str++))
+            return false;
+    return true;
+}
+
 int main(int argc, char *argv[]) 
 { 
-    char x; 
-    cin >> x; 
-    if (islower(x)){
-        cout << "This is an lower case letter."; 
+    if (argc != 2 || legal_num(argv[1]) == true){
+        cout << "Please only input letters for this program." << endl;
+        return 0;
+    }
+    if( (legal_int(argv[1]) == true) ){
+        cout << "This is an lower case letter." << endl; 
         return -1;}
     else{
-        cout << "This is a upper case letter.";     
+        cout << "This is a upper case letter." << endl;     
         return 1;}
 }
